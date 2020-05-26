29a98d3089a8c30a6ea49b0198760a8cde9719f1
Navya730 <65384137+Navya730@users.noreply.github.com>
2020-05-26 21:42:53 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,11 @@
+(function (window){
+var names=["Yanko", "John", "Jason", "Jen", " Paul"];
+for(var i=0;i<5;i++) 
+if(firstletter=='J'){
+window.byespeaker.speak(names[i]);
+else{
+window.hellospeaker.speak(names[i]);
+}
+}
+}(window);
+
