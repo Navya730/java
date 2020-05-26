1997e2f8462eef08d2bc80dee80791fa36b3de9a
Navya730 <65384137+Navya730@users.noreply.github.com>
2020-05-26 21:14:04 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 (function (window) {
 var names=[{name:"John"},{ name:"nav"}, {name:"Joe"}];
 var speakWord = "Hello";
-names.sayhello=function=function () {
+names.sayhello=function () {
   console.log(speakWord + names.name);
 }
 window.names=names;
